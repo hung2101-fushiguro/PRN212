22b6b127f89a8cdf4ff667a73b7b5fc3a76e454d
Satoru <joonheung2101@gmail.com>
2026-01-09 11:34:51 +0700

    PRN212

---
+++ console/numericLiteral/numericLiteral/Program.cs
@@ -0,0 +1,17 @@
+﻿class NumericLiteral
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("**** Use Digit Sperators ****");
+        Console.Write("Integer: ");
+        Console.WriteLine(123_456);
+        Console.Write("Double :");
+        Console.WriteLine(123_456.12);
+        Console.Write("Hex: ");
+        Console.WriteLine(0x_00_00_FF);
+        Console.WriteLine("**** Use Binary Literals ****");
+        Console.WriteLine("Sixteen : {0}", 0b0001_0000);
+        Console.ReadLine();
+
+    }
+}
---
+++ tuples/Tuple/Tuple/Main.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Tuple
+{
+    internal class Program
+    {
+        static(int sum, int count) MyMethod(int[] values)
+        {
+            var r = (sum: 0, count: 0);
+            for(int i = 0; i< values.Length; i++)
+            {
+                if(IsEvenNumber(values[i]))
+                {
+                    r.sum += values[i];
+                    r.count++;
+                }
+            }
+            return r;
+            bool IsEvenNumber(int value)
+            {
+                return value % 2 == 0;
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var (sum, count) = MyMethod(numbers);
+            Console.WriteLine($"Sum: {sum}, Count:{count}");
+            Console.ReadLine();
+        }
+    }
+}
